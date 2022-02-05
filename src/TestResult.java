9afacf2260bda18e05db31867c9fcf4b2727736a
Artem Teslia <91652654+eSquire-qq@users.noreply.github.com>
2022-02-05 15:18:32 +0200

    Update TestResult.java

--- src/TestResult.java
+++ src/TestResult.java
@@ -1,25 +1,6 @@
 public class TestResult {
 
-    public static void main(String[] args) {
-    
-    int number = 10;
-    boolean condition = true;
-
-        for (int counter1 = 2; counter1 <= number; counter1++) {
-              for (int counter2 = 2; counter2 < counter1; counter2++) {
-                  if (counter1 % counter2 == 0) {
-                    condition = false;
-                    break;
-                  }
-         }
-         if (condition) {
-            System.out.print(counter1 + ",");
-         } 
-         
-         else {
-            condition = true;
-         }
-
-      }
+    public static void main(int counter1) {
+            return counter1;
     }
 }
